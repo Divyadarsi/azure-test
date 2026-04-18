bf176e8dddcedbc64f3683a7071bda5105da48d7
Divyadarsi <49521974+Divyadarsi@users.noreply.github.com>
2026-04-19 01:11:53 +0530

    Refactor resource group variables in variables.tf
    Updated resource group variables and default values.

--- variables.tf
+++ variables.tf
@@ -1,17 +1,15 @@
-variable "resource_group_location" {
-  type        = string
-  default     = "eastus"
-  description = "Location of the resource group."
+variable "resource_group_name_prefix" {
+  default = "rg"
 }
 
-variable "resource_group_name_prefix" {
-  type        = string
-  default     = "rg"
-  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "resource_group_location" {
+  default = "East US"
 }
 
 variable "username" {
-  type        = string
-  description = "The username for the local account that will be created on the new VM."
-  default     = "azureadmin"
+  default = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "SSH Public Key"
 }
