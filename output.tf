9eaaa64d7d28893353f933ddbada4e39f8cafa7a
Divyadarsi <49521974+Divyadarsi@users.noreply.github.com>
2026-04-19 01:12:28 +0530

    Refactor output variables in output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.my_terraform_vm.public_ip_address
+output "public_ip" {
+  value = azurerm_public_ip.my_terraform_public_ip.ip_address
 }
