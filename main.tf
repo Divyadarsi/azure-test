e8da1599ac7cd5e854fbb80a07a8b640cb50638a
Divyadarsi <49521974+Divyadarsi@users.noreply.github.com>
2026-04-23 17:34:21 +0530

    Fix syntax for admin_ssh_key block in main.tf

--- main.tf
+++ main.tf
@@ -113,7 +113,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   computer_name  = "hostname"
   admin_username = var.username
 
-admin_ssh_key {
+  admin_ssh_key {
     username   = var.username
     public_key = var.ssh_public_key
   }
